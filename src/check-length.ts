fbc38318dfb0b3a27ecdc61ad89b51808f9c4f96
Евгений Войтенко <e.voytenko@tinkoff.ru>
2023-08-18 14:17:38 +0300

    ref typescript

--- src/check-length.ts
+++ src/check-length.ts
@@ -1,4 +1,5 @@
-import { TSESLint } from "@typescript-eslint/utils";
+import { TSESLint, TSESTree } from "@typescript-eslint/utils";
+import { AST_NODE_TYPES } from "@typescript-eslint/types";
 
 export const rule: TSESLint.RuleModule<"someError"> = {
   defaultOptions: [],
@@ -18,26 +19,27 @@ export const rule: TSESLint.RuleModule<"someError"> = {
   // тело правила
   create(context) {
     return {
-      JSXOpeningElement(node: any) {
-        const attributes = node.attributes;
+      JSXOpeningElement(node: TSESTree.JSXOpeningElement) {
+        const attributes = node.attributes as TSESTree.JSXAttribute[];
         if (attributes) {
-          attributes.forEach((attribute) => {
-            const value = attribute?.value;
-            const expression = value?.expression;
-            const obj = expression?.object;
-            if (obj?.type === "TSNonNullExpression") {
+          attributes.forEach((attribute: TSESTree.JSXAttribute) => {
+            const value = attribute.value as TSESTree.JSXExpressionContainer;
+            const expression =
+              value.expression as TSESTree.Expression as TSESTree.MemberExpression;
+            const obj = expression?.object as TSESTree.TSNonNullExpression;
+            if (obj?.type === AST_NODE_TYPES.TSNonNullExpression) {
               const loc = {
                 line: obj.loc.start.line,
                 column: obj.loc.end.column - 1,
               };
 
-              const range = obj.expression.range;
+              const range: TSESTree.Range = obj.expression.range;
 
               context.report({
                 loc: loc,
                 messageId: "someError",
                 // фикс кода
-                fix: (fixer) => {
+                fix: (fixer: TSESLint.RuleFixer) => {
                   return fixer.replaceTextRange([range[1], range[1] + 1], "?");
                 },
               });
