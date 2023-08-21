864f9ec597424e26e56fc0995dd5cbca7f85a44a
Евгений Войтенко <e.voytenko@tinkoff.ru>
2023-08-21 16:25:43 +0300

    ref rule export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,59 @@
-import {rule} from "./check-length";
+import { TSESLint } from "@typescript-eslint/utils";
 
-export const rules = {
-    'check-length': rule,
-}
+export const rules: {
+  "check-length": TSESLint.RuleModule<"someError", { min: number }[]>;
+} = {
+  "check-length": {
+    defaultOptions: [],
+    // информация о правиле
+    meta: {
+      // типы "problem", "suggestion" или "layout"
+      type: "suggestion",
+      // правило может менять код
+      fixable: "code",
+      // тут описываются аргументы правила
+      schema: [
+        {
+          type: "object",
+          properties: {
+            min: {
+              type: "number",
+            },
+          },
+          additionalProperties: false,
+        },
+      ],
+      // текст ошибки при репорте
+      messages: {
+        someError: "Длина аргумента функции должна быть больше",
+      },
+    },
+    // тело правила
+    create(context) {
+      return {
+        CallExpression(node: any) {
+          const { callee } = node;
+          //const { arguments } = node;
+          if (callee.name === "someFunction") {
+            const argument = node.arguments[0].value;
+            const firstArgument = node.arguments[0];
+            // @ts-ignore
+            const min = context.options[0]?.min || 2;
+            if (argument.length < min) {
+              context.report({
+                node,
+                messageId: "someError",
+                fix(fixer) {
+                  return fixer.insertTextAfterRange(
+                    [firstArgument.range[0], firstArgument.range[1] - 1],
+                    "+"
+                  );
+                },
+              });
+            }
+          }
+        },
+      };
+    },
+  },
+};
--- test/test.spec.ts
+++ test/test.spec.ts
@@ -1,10 +1,10 @@
 import { RuleTester } from "@typescript-eslint/rule-tester";
-import { rule } from "../src/check-length";
+import { rules } from "../src";
 
 const ruleTester = new RuleTester();
 
 // передаем название правила, само правило, тесты
-ruleTester.run("check-length", rule, {
+ruleTester.run("check-length", rules["check-length"], {
   // успешный тест
   valid: [
     {
