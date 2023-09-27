0eb34601ee49e1a1d563b282858465a76faee750
Евгений Войтенко <e.voytenko@tinkoff.ru>
2023-09-27 11:49:40 +0300

    fix patch

--- src/nonull-assert.ts
+++ src/nonull-assert.ts
@@ -19,33 +19,24 @@ export const rule: TSESLint.RuleModule<"someError"> = {
   // тело правила
   create(context) {
     return {
-      JSXOpeningElement(node: TSESTree.JSXOpeningElement) {
-        const attributes = node.attributes as TSESTree.JSXAttribute[];
-        if (attributes) {
-          attributes.forEach((attribute: TSESTree.JSXAttribute) => {
-            const value = attribute.value as TSESTree.JSXExpressionContainer;
-            const expression =
-              value.expression as TSESTree.Expression as TSESTree.MemberExpression;
-            const obj = expression?.object as TSESTree.TSNonNullExpression;
-            if (obj?.type === AST_NODE_TYPES.TSNonNullExpression) {
-              const loc = {
-                line: obj.loc.start.line,
-                column: obj.loc.end.column - 1,
-              };
+      "JSXExpressionContainer > MemberExpression > TSNonNullExpression"(
+        node: TSESTree.TSNonNullExpression
+      ) {
+        const loc = {
+          line: node.loc.start.line,
+          column: node.loc.end.column - 1,
+        };
 
-              const range: TSESTree.Range = obj.expression.range;
+        const range: TSESTree.Range = node.expression.range;
 
-              context.report({
-                loc: loc,
-                messageId: "someError",
-                // фикс кода
-                fix: (fixer: TSESLint.RuleFixer) => {
-                  return fixer.replaceTextRange([range[1], range[1] + 1], "?");
-                },
-              });
-            }
-          });
-        }
+        context.report({
+          loc: loc,
+          messageId: "someError",
+          // фикс кода
+          fix: (fixer: TSESLint.RuleFixer) => {
+            return fixer.replaceTextRange([range[1], range[1] + 1], "?");
+          },
+        });
       },
     };
   },
